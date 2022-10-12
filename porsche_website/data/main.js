b87e716e05acb62bc526d820f635c2fb11f65fef
Tim Mewe <tim.mewe@jambit.com>
2022-10-12 15:21:33 +0200

    Add porsche animation

---
+++ porsche_website/data/main.js
@@ -0,0 +1,31 @@
+var startPosition = '-1000px';
+var animate;
+
+function init() {
+    imgObj = document.getElementById('porsche');
+    imgObj.style.position = 'relative';
+    imgObj.style.left = startPosition;
+    // imgObj.style.visibility = 'hidden';
+}
+
+function startAnimation() {
+    stopAnimation();
+    imgObj.style.left = startPosition;
+    animateCar()
+}
+
+function animateCar() {
+    if (parseInt(imgObj.style.left) > 3000) {
+        stopAnimation()
+        return;
+    }
+
+    imgObj.style.left = parseInt(imgObj.style.left) + 20 + 'px';
+    animate = setTimeout(animateCar, 20);
+}
+
+function stopAnimation() {
+    clearTimeout(animate);
+}
+
+window.onload = function () { init(); };
